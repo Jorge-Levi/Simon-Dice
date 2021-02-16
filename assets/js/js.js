2d706d6bbd3520fe7e6c559c71bc3e1007bacf03
= <=>
2021-02-16 08:21:56 -0600

    añadiendo iluminacion de colores

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,11 +1,62 @@
 class Juego{
     constructor(){
         this.empezarJuego();
+        this.secuencia();
+        this.siguienteNivel();
     }
 
     empezarJuego(){
         // alert("Hola Mundo");
         btnEmpezar.classList.add("hide");
+        this.nivel = 7;
+        this.colores = {
+            celeste,
+            violeta,
+            naranja,
+            verde
+        }
+    }
+
+    secuencia(){
+        this.secuencia = new Array(10).fill(0).map(n => Math.floor(Math.random() * 4));
+    }
+
+    siguienteNivel(){  
+        this.iluminarSecuencia();
+    }
+
+    transformarNumeroColor(numero){
+        switch (numero) {
+            case 0:
+                return 'celeste';
+            case 1:
+                return 'violeta';
+            case 2:
+                return 'naranja';
+            case 3:
+                return 'verde';
+            default:
+                break;
+        }
+    }
+
+    
+    iluminarSecuencia(){
+        for(let i = 0 ; i < this.nivel ; i++){
+            let color = this.transformarNumeroColor(this.secuencia[i]);
+            setTimeout(() => this.iluminarColor(color),1000 * i);
+        }
+    }
+    
+    iluminarColor(color){
+        this.colores[color].classList.add('light');
+        console.log(`iluminando ${color}`);
+        setTimeout(() => this.apagarColor(color),350);
+    }
+    
+    apagarColor(color){
+        this.colores[color].classList.remove('light');
+        console.log(`apagando ${color}`);
     }
 }
 
@@ -16,5 +67,5 @@ let naranja = document.getElementById("naranja");
 let verde = document.getElementById("verde");
 
 function empezarJuego() {
-    var nuevoJuego = new Juego();
+     window.nuevoJuego = new Juego();
 }
